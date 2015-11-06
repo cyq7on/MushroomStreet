88be2af648ed6ef766434aa2629f3ebc98d00530
cyq7on <cyq7on@84578760-8376-4870-a0f4-681ebd1974c2>
2015-11-06 05:21:27 +0000

    下拉刷新

---
+++ trunk/mushroomStreet-master/src/com/cyq7on/mushrommstreet/adapter/DynamicAdapter.java
@@ -0,0 +1,319 @@
+package com.cyq7on.mushrommstreet.adapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cyq7on.mushrommstreet.bean.DynamicVo;
+import com.cyq7on.mushrommstreet.utils.DeviceInfo;
+import com.cyq7on.mushroomstreet.AppConfig;
+import com.example.mushroomstreet.R;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+public class DynamicAdapter extends BaseAdapter {
+
+	private LayoutInflater mInflater;
+	private List<DynamicVo> list;
+	private Context mContext;
+
+
+	public DynamicAdapter(Context context, List<DynamicVo> list) {
+		this.list = list;
+		this.mInflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		this.mContext = context;
+	}
+
+
+	@Override
+	public int getCount() {
+		 return list.size();
+	}
+
+	@Override
+	public DynamicVo getItem(int position) {
+		return list.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		final ViewHolder holder;
+		if (convertView == null) {
+			holder = new ViewHolder();
+
+			convertView = mInflater.inflate(
+					R.layout.mime_share_item, parent, false);
+			holder.ivUser = (ImageView) convertView
+					.findViewById(R.id.iv_user);
+			holder.tvUserName = (TextView) convertView
+					.findViewById(R.id.username);
+			holder.tvDate = (TextView) convertView
+					.findViewById(R.id.contenttime);
+			holder.btnAttention = (Button) convertView.
+					findViewById(R.id.btn_attention);
+			holder.llContentImage = (LinearLayout) convertView
+					.findViewById(R.id.image_container);
+			holder.btnGoods = (Button) convertView.
+					findViewById(R.id.btn_goods);
+			holder.ivDianzan = (ImageView) convertView
+					.findViewById(R.id.iv_dianzan);
+			holder.tvDianzanNum = (TextView) convertView
+					.findViewById(R.id.tv_dianzan);
+			holder.btnComment = (Button) convertView.
+					findViewById(R.id.btn_comment);
+			holder.tvLookAll = (TextView) convertView
+					.findViewById(R.id.tv_comment_num);
+			holder.tvCommentPeople = (TextView) convertView
+					.findViewById(R.id.tv_comment_people);
+			holder.tvContent = (TextView) convertView
+					.findViewById(R.id.tv_comment);
+			convertView.setTag(holder);
+//			convertView.setId(position);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+
+		int width = (int)DeviceInfo.getScreenWidth(mContext);// 屏幕宽度
+		if (list.get(position).getContentImageurl().size() == 1) {
+			holder.llContentImage.removeAllViews();
+			ImageView image1 = new ImageView(mContext);
+			image1.setScaleType(ScaleType.FIT_XY);
+			int hight = (int) DeviceInfo.getScreenHight(mContext);// 屏幕高度
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+					LinearLayout.LayoutParams.MATCH_PARENT, hight / 4);
+			holder.llContentImage.addView(image1, params);
+			ImageLoader.getInstance().displayImage(list.get(position).getContentImageurl().get(0),
+					image1, AppConfig.options);
+		} else if (list.get(position).getContentImageurl().size() == 2) {
+			holder.llContentImage.removeAllViews();
+
+			LinearLayout horizonLayout = new LinearLayout(mContext);
+			LinearLayout.LayoutParams params;
+			float density = DeviceInfo.getDensity(mContext);
+			float imageLayWidth = width - (10 + 10) * density;
+			for (int i = 0; i < list.get(position).getContentImageurl().size(); i++) {
+				params = new LinearLayout.LayoutParams(
+						(int) (imageLayWidth / 2), (int) (imageLayWidth / 2));
+				ImageView image2 = new ImageView(mContext);
+				image2.setScaleType(ScaleType.FIT_XY);
+				image2.setPadding(0, (int) (4 * density), (int) (4 * density),
+						(int) (4 * density));
+				horizonLayout.addView(image2, params);
+				ImageLoader.getInstance().displayImage(
+						list.get(position).getContentImageurl().get(i), image2, AppConfig.options);
+			}
+			holder.llContentImage.addView(horizonLayout);
+		} else if (list.get(position).getContentImageurl().size() > 2 
+				&& list.get(position).getContentImageurl().size() <= 9) {
+			holder.llContentImage.removeAllViews();
+
+			LinearLayout.LayoutParams params;
+			float density = DeviceInfo.getDensity(mContext);
+			float imageLayWidth = width - (10 + 10) * density;
+
+			int size = list.get(position).getContentImageurl().size();
+			int yuShu = size % 3;
+			if (yuShu == 0) {
+				int hangNum = (int) (size / 3);
+				for (int i = 0; i < hangNum; i++) {
+					LinearLayout horizonLayout = new LinearLayout(mContext);
+					for (int j = 0; j < 3; j++) {
+						params = new LinearLayout.LayoutParams(
+								(int) (imageLayWidth / 3),
+								(int) (imageLayWidth / 3));
+						ImageView image3 = new ImageView(mContext);
+						image3.setScaleType(ScaleType.FIT_XY);
+						image3.setPadding(0, (int) (2 * density),
+								(int) (2 * density), (int) (2 * density));
+						horizonLayout.addView(image3, params);
+						ImageLoader.getInstance().displayImage(
+								list.get(position).getContentImageurl().get(i * 3 + j),image3,
+								AppConfig.options);
+					}
+					holder.llContentImage.addView(horizonLayout);
+				}
+			} else {
+				int hangNum = (int) (size / 3) + 1;
+				for (int i = 0; i <= hangNum - 1; i++) {
+					LinearLayout horizonLayout = new LinearLayout(mContext);
+					if (i < hangNum - 1) {
+						for (int j = 0; j < 3; j++) {
+							params = new LinearLayout.LayoutParams(
+									(int) (imageLayWidth / 3),
+									(int) (imageLayWidth / 3));
+							ImageView image3 = new ImageView(mContext);
+							image3.setPadding(0, (int) (2 * density),
+									(int) (2 * density), (int) (2 * density));
+							image3.setScaleType(ScaleType.FIT_XY);
+							horizonLayout.addView(image3, params);
+							ImageLoader.getInstance().displayImage(
+									list.get(position).getContentImageurl().get(i * 3 + j), image3,
+									AppConfig.options);
+						}
+						holder.llContentImage.addView(horizonLayout);
+					} else if (i == hangNum - 1) {
+						for (int j = 0; j < yuShu; j++) {
+							params = new LinearLayout.LayoutParams(
+									(int) (imageLayWidth / 3),
+									(int) (imageLayWidth / 3));
+							ImageView image3 = new ImageView(mContext);
+							image3.setScaleType(ScaleType.FIT_XY);
+							horizonLayout.addView(image3, params);
+							ImageLoader.getInstance().displayImage(
+									list.get(position).getContentImageurl().get(i * 3 + j), image3,
+									AppConfig.options);
+						}
+						holder.llContentImage.addView(horizonLayout);
+					}
+				}
+			}
+		}
+		
+		try {
+			ImageLoader.getInstance().displayImage(list.get(position).getUrl(), 
+					holder.ivUser, 
+					AppConfig.options);
+			
+			holder.tvUserName.setText(list.get(position).getUserName());
+			holder.tvDate.setText(list.get(position).getDate());
+			holder.tvPlace.setText(list.get(position).getTvPlace());
+			holder.tvContent.setText(list.get(position).getTvContent());
+			holder.btnGoods.setText(list.get(position).getGoods()+"件相关商品");
+			holder.tvDianzanNum.setText(list.get(position).getDianzanNum());
+			holder.tvLookAll.setText("查看所有"+list.get(position).getComment()+"条评论");
+			holder.tvCommentPeople.setText(list.get(position).getCommentPeople());
+			holder.tvComment.setText(list.get(position).getContent());
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		
+		holder.tvUserName.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, list.get(position).getUserName(),
+						Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		holder.ivUser.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, list.get(position).getUserName(),
+						Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		
+		holder.btnAttention.setOnClickListener(new OnClickListener() {
+			boolean isAttention = false;
+			@Override
+			public void onClick(View v) {
+				if (!isAttention) {
+					holder.btnAttention.setText("已关注");
+					holder.btnAttention.setTextColor(
+							android.graphics.Color.DKGRAY);
+					isAttention = true;
+				} else {
+					holder.btnAttention.setText("+ 关注");
+					holder.btnAttention.setTextColor(
+							Color.parseColor("#000000"));
+					isAttention = false;
+				}
+			}
+		});
+		
+		holder.ivDianzan.
+		setBackgroundResource(R.drawable.like_icon_grey);
+		holder.ivDianzan.setOnClickListener(new OnClickListener() {
+			boolean b = false;
+			@Override
+			public void onClick(View v) {
+				if (!b) {
+					holder.ivDianzan.
+					setBackgroundResource(R.drawable.like_icon_red);
+					b = true;
+				} else {
+					holder.ivDianzan.
+					setBackgroundResource(R.drawable.like_icon_grey);
+					b = false;
+				}
+			}
+		});
+		
+		holder.btnGoods.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, "评论" + list.get(position).getGoods(),
+						Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		holder.btnComment.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, "评论" + list.get(position).getUserName(),
+						Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		holder.tvLookAll.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, "查看所有评论",
+						Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		holder.tvCommentPeople.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, "我是:"+list.get(position).
+						getCommentPeople(),
+						Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		return convertView;
+	}
+
+	private class ViewHolder {
+		public ImageView ivUser;//用戶头像
+		public TextView tvUserName;//用户名
+		public TextView tvDate;//动态时间
+		public TextView tvPlace;//动态地址
+		public Button btnAttention;//关注按钮
+		public TextView tvContent;//动态内容
+		public LinearLayout llContentImage;//承载动态里图片的线性布局
+		public Button btnGoods;//相关商品
+		public ImageView ivDianzan;//点赞图标
+		public TextView tvDianzanNum;//点赞数量
+		public Button btnComment;//评论按钮
+		public TextView tvLookAll;//查看所有评论
+		public TextView tvCommentPeople;//评论人
+		public TextView tvComment;//评论内容
+	}
+}
--- trunk/mushroomStreet-master/src/com/cyq7on/mushrommstreet/fragment/ShoppingFragment.java
+++ trunk/mushroomStreet-master/src/com/cyq7on/mushrommstreet/fragment/ShoppingFragment.java
@@ -1,13 +1,14 @@
 package com.cyq7on.mushrommstreet.fragment;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.view.PagerAdapter;
@@ -27,16 +28,19 @@ import android.widget.ImageView.ScaleType;
 import android.widget.Toast;
 
 import com.cyq7on.mushrommstreet.activity.MainActivity;
+import com.cyq7on.mushrommstreet.adapter.DynamicAdapter;
+import com.cyq7on.mushrommstreet.bean.DynamicVo;
 import com.cyq7on.mushrommstreet.shoppingfragment.activity.SearchActivity;
 import com.cyq7on.mushrommstreet.view.HorizontalListView;
+import com.cyq7on.mushrommstreet.view.XListView;
+import com.cyq7on.mushrommstreet.view.XListView.IXListViewListener;
 import com.cyq7on.mushroomstreet.AppConfig;
 import com.example.mushroomstreet.R;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.viewpagerindicator.CirclePageIndicator;
 
-
-public class ShoppingFragment extends BasicFragment implements OnClickListener{
+public class ShoppingFragment extends BasicFragment implements
+		IXListViewListener, OnClickListener {
 
 	private ImageView ivAdd;
 	private EditText etSearch;
@@ -44,20 +48,24 @@ public class ShoppingFragment extends BasicFragment implements OnClickListener{
 	private List<ImageView> list = new ArrayList<ImageView>();
 	private CirclePageIndicator circlePageIndicator;
 	private Handler mHandler;
-	private int which;//哪个图片被选中
+	private int which;// 哪个图片被选中
 	private HorizontalListView horizontalListView;
-	private int[] source = {R.drawable.ic_launcher,R.drawable.index_follow_icon
-			,R.drawable.index_my_icon};
-	//轮播图片地址
-	private String [] url = {
+	private int[] source = { R.drawable.ic_launcher,
+			R.drawable.index_follow_icon, R.drawable.index_my_icon };
+	private XListView xListview;//动态listview
+	private DynamicAdapter adapter;//动态适配器
+	private List<DynamicVo> listDynamic = new ArrayList<DynamicVo>();
+
+	// 轮播图片地址
+	private String[] url = {
 			"https://img.alicdn.com/tps/TB1Z1siKXXXXXcvXVXXXXXXXXXX-400-200.jpg",
 			"https://img.alicdn.com/tps/TB16nMPKXXXXXcQXVXXXXXXXXXX-400-200.jpg",
 			"https://img.alicdn.com/tps/TB1hMIUKXXXXXakXVXXXXXXXXXX-400-200.jpg",
 			"https://img.alicdn.com/tps/TB1MsspKXXXXXcUXFXXXXXXXXXX-400-200.jpg",
 			"https://img.alicdn.com/bao/uploaded/i1/TB1OwwiKXXXXXaQXpXXSutbFXXX.jpg",
-			"https://img.alicdn.com/bao/uploaded/i1/TB1XzItKXXXXXbLXVXXSutbFXXX.jpg"};
-	//滑动图片地址
-	private String [] url1 = {
+			"https://img.alicdn.com/bao/uploaded/i1/TB1XzItKXXXXXbLXVXXSutbFXXX.jpg" };
+	// 滑动图片地址
+	private String[] url1 = {
 			"https://img.alicdn.com/tps/TB1Z1siKXXXXXcvXVXXXXXXXXXX-400-200.jpg",
 			"https://img.alicdn.com/tps/TB16nMPKXXXXXcQXVXXXXXXXXXX-400-200.jpg",
 			"https://img.alicdn.com/tps/TB1hMIUKXXXXXakXVXXXXXXXXXX-400-200.jpg",
@@ -65,110 +73,200 @@ public class ShoppingFragment extends BasicFragment implements OnClickListener{
 			"https://img.alicdn.com/tps/TB1Z1siKXXXXXcvXVXXXXXXXXXX-400-200.jpg",
 			"https://img.alicdn.com/tps/TB16nMPKXXXXXcQXVXXXXXXXXXX-400-200.jpg",
 			"https://img.alicdn.com/tps/TB1hMIUKXXXXXakXVXXXXXXXXXX-400-200.jpg",
-			"https://img.alicdn.com/tps/TB1MsspKXXXXXcUXFXXXXXXXXXX-400-200.jpg"};
+			"https://img.alicdn.com/tps/TB1MsspKXXXXXcUXFXXXXXXXXXX-400-200.jpg" };
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		view = inflater.inflate(R.layout.fragment_shopping, container, false);
-		
+		initData();
 		initView();
 		return view;
 	}
-	
+
+	@Override
+	public void initData() {
+		for (int i = 0; i < 15; i++) {
+			// 这里为了简便就添加了部分信息
+			DynamicVo data = new DynamicVo();
+			data.setUserName("花千骨" + i);
+			data.setUrl("http://www.qq1234."
+					+ "org/uploads/allimg/150706/8_150706145211_9.jpg");
+			SimpleDateFormat sdf = new SimpleDateFormat("MM-dd HH:mm:ss");
+			data.setDate(sdf.format(new Date()));
+			data.setTvPlace("成都" + i);
+			List<String> urlList = new ArrayList<String>();
+			for (int j = 0; j <= i; j++) {
+				urlList.add(url[j % url.length]);
+			}
+			data.setContentImageurl(urlList);
+			listDynamic.add(data);
+		}
+	}
+
 	@Override
 	public void initView() {
 		activity = (MainActivity) getActivity();
 		ivAdd = (ImageView) view.findViewById(R.id.iv_add);
 		etSearch = (EditText) view.findViewById(R.id.et_search);
-		
+
 		ivAdd.setOnClickListener(this);
 		etSearch.setOnClickListener(this);
 		initViewPager();
 		initHorizontalListView();
-		
+		initXListView();
 	}
-	
+
+	private void initXListView() {
+		xListview = (XListView) view.findViewById(R.id.xlistview);
+		View headerView = View.inflate(activity, R.layout.list_header, null);
+		xListview.addHeaderView(headerView);
+
+		xListview.setPullLoadEnable(true);
+		xListview.setXListViewListener(this);
+		xListview.setRefreshTime(new Date().toLocaleString());
+		adapter = new DynamicAdapter(activity, listDynamic);
+		xListview.setAdapter(adapter);
+
+		xListview.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				Toast.makeText(activity, arg2 + "", Toast.LENGTH_LONG).show();
+			}
+		});
+	}
+
+	// 下拉刷新
+	@Override
+	public void onRefresh() {
+		listDynamic.clear();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd HH:mm:ss");
+		xListview.setRefreshTime(sdf.format(new Date()));
+		for (int i = 0; i < 15; i++) {
+			// 这里为了简便就添加了部分信息
+			DynamicVo data = new DynamicVo();
+			data.setUserName("花千骨" + i + "下拉刷新");
+			data.setUrl("http://www.qq1234."
+					+ "org/uploads/allimg/150706/8_150706145211_9.jpg");
+			data.setDate(sdf.format(new Date()));
+			data.setTvPlace("成都" + i);
+			List<String> urlList = new ArrayList<String>();
+			for (int j = 0; j <= i; j++) {
+				urlList.add(url[j % url.length]);
+			}
+			data.setContentImageurl(urlList);
+			listDynamic.add(data);
+		}
+		adapter.notifyDataSetChanged();
+		xListview.stopRefresh();
+	}
+
+	// 上拉加载更多
+	@Override
+	public void onLoadMore() {
+		for (int i = 0; i < 15; i++) {
+			// 这里为了简便就添加了部分信息
+			DynamicVo data = new DynamicVo();
+			data.setUserName("花千骨" + i + "加载更多");
+			data.setUrl("http://www.qq1234."
+					+ "org/uploads/allimg/150706/8_150706145211_9.jpg");
+			SimpleDateFormat sdf = new SimpleDateFormat("MM-dd HH:mm:ss");
+			data.setDate(sdf.format(new Date()));
+			data.setTvPlace("成都" + i);
+			List<String> urlList = new ArrayList<String>();
+			for (int j = 0; j <= i; j++) {
+				urlList.add(url[j % url.length]);
+			}
+			data.setContentImageurl(urlList);
+			listDynamic.add(data);
+		}
+		adapter.notifyDataSetChanged();
+		xListview.stopRefresh();
+	}
+
 	private void initHorizontalListView() {
-		
-		horizontalListView = (HorizontalListView) 
-				view.findViewById(R.id.hl_iv);
-		 horizontalListView.setAdapter(new BaseAdapter() {
-				
-				@Override
-				public View getView(int position, View convertView, ViewGroup parent) {
-					ImageView iv;
-					if (convertView == null) {
-						convertView = LayoutInflater.from(activity).inflate(
-								R.layout.item_hlistview, null);
-						iv = (ImageView) convertView.findViewById(R.id.iv);
-						convertView.setTag(iv);
-					}else {
-						iv = (ImageView) convertView.getTag();
-					}
-					ImageLoader.getInstance().displayImage(url1[position],
-					iv, AppConfig.options);
-					return convertView;
+
+		horizontalListView = (HorizontalListView) view.findViewById(R.id.hl_iv);
+		horizontalListView.setAdapter(new BaseAdapter() {
+
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				ImageView iv;
+				if (convertView == null) {
+					convertView = LayoutInflater.from(activity).inflate(
+							R.layout.item_hlistview, null);
+					iv = (ImageView) convertView.findViewById(R.id.iv);
+					convertView.setTag(iv);
+				} else {
+					iv = (ImageView) convertView.getTag();
 				}
-				
-				@Override
-				public long getItemId(int position) {
-					// TODO Auto-generated method stub
-					return 0;
-				}
-				
-				@Override
-				public Object getItem(int position) {
-					// TODO Auto-generated method stub
-					return null;
-				}
-				
-				@Override
-				public int getCount() {
-					// TODO Auto-generated method stub
-					return url1.length;
-				}
-			});
-		 
-		
+				ImageLoader.getInstance().displayImage(url1[position], iv,
+						AppConfig.options);
+				return convertView;
+			}
+
+			@Override
+			public long getItemId(int position) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public Object getItem(int position) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public int getCount() {
+				// TODO Auto-generated method stub
+				return url1.length;
+			}
+		});
+
 		horizontalListView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
-				Toast.makeText(activity, arg2+"", Toast.LENGTH_LONG).show();
+				Toast.makeText(activity, arg2 + "", Toast.LENGTH_LONG).show();
 			}
 		});
 	}
-	
+
 	private void initViewPager() {
 		vpTop = (ViewPager) view.findViewById(R.id.vp_top);
-		circlePageIndicator = (CirclePageIndicator) 
-				view.findViewById(R.id.circlepageindicator);
+		circlePageIndicator = (CirclePageIndicator) view
+				.findViewById(R.id.circlepageindicator);
 		ImageView iv;
 		for (int i = 0; i < url.length; i++) {
-	        iv = new ImageView(activity);  
-	        iv.setOnClickListener(new OnClickListener() {
-				
+			iv = new ImageView(activity);
+			iv.setOnClickListener(new OnClickListener() {
+
 				@Override
 				public void onClick(View v) {
-					Toast.makeText(activity, "您选择了第"+(which+1)+"张", Toast.LENGTH_LONG).show();
+					Toast.makeText(activity, "您选择了第" + (which + 1) + "张",
+							Toast.LENGTH_LONG).show();
 				}
 			});
-	        ImageLoader.getInstance().displayImage(url[i], iv, AppConfig.options);  
+			ImageLoader.getInstance().displayImage(url[i], iv,
+					AppConfig.options);
 			list.add(iv);
 		}
 		vpTop.setAdapter(new PagerAdapter() {
-			
+
 			@Override
 			public boolean isViewFromObject(View arg0, Object arg1) {
 				return arg0 == arg1;
 			}
-			
+
 			@Override
 			public int getCount() {
 				return list.size();
 			}
-			
+
 			@Override
 			public Object instantiateItem(ViewGroup container, int position) {
 				ImageView iv = list.get(position);
@@ -176,33 +274,34 @@ public class ShoppingFragment extends BasicFragment implements OnClickListener{
 				container.addView(iv);
 				return iv;
 			}
-			
+
 			@Override
-			public void destroyItem(ViewGroup container, int position, Object object) {
+			public void destroyItem(ViewGroup container, int position,
+					Object object) {
 				container.removeView((View) object);
 			}
 		});
-		
+
 		circlePageIndicator.setViewPager(vpTop);
 		circlePageIndicator.setOnPageChangeListener(new OnPageChangeListener() {
-			
+
 			@Override
 			public void onPageSelected(int arg0) {
 				which = arg0;
 			}
-			
+
 			@Override
 			public void onPageScrolled(int arg0, float arg1, int arg2) {
-				
+
 			}
-			
+
 			@Override
 			public void onPageScrollStateChanged(int arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
-		
+
 		// 自动轮播条显示
 		if (mHandler == null) {
 			mHandler = new Handler() {
@@ -223,31 +322,32 @@ public class ShoppingFragment extends BasicFragment implements OnClickListener{
 			mHandler.sendEmptyMessageDelayed(0, 3000);// 延时3秒后发消息
 		}
 	}
-	
+
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.iv_add:
 			Builder builder = new Builder(activity);
-			final String[] items = { "查找好友", "扫一扫"};
+			final String[] items = { "查找好友", "扫一扫" };
 			builder.setItems(items, new DialogInterface.OnClickListener() {
-				
+
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					Toast.makeText(activity, items[which], Toast.LENGTH_LONG).show();
+					Toast.makeText(activity, items[which], Toast.LENGTH_LONG)
+							.show();
 				}
 			});
 			AlertDialog alertDialog = builder.create();
 			Window win = alertDialog.getWindow();
-//			LayoutParams params = new LayoutParams();
-//			params.x = 80;//设置x坐标
-//			params.y = 60;//设置y坐标
-//			win.setAttributes(params);
+			// LayoutParams params = new LayoutParams();
+			// params.x = 80;//设置x坐标
+			// params.y = 60;//设置y坐标
+			// win.setAttributes(params);
 			alertDialog.show();
 			break;
 
 		case R.id.et_search:
-			startActivity(new Intent(activity,SearchActivity.class));
+			startActivity(new Intent(activity, SearchActivity.class));
 		default:
 			break;
 		}

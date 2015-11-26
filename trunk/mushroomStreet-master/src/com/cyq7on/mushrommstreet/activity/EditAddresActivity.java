b74db661cc14a3ebb1aa5e16e60293de85f8f5ff
cyq7on <cyq7on@84578760-8376-4870-a0f4-681ebd1974c2>
2015-11-26 13:50:42 +0000

    编辑收货地址页面

--- trunk/mushroomStreet-master/src/com/cyq7on/mushrommstreet/activity/EditAddresActivity.java
+++ trunk/mushroomStreet-master/src/com/cyq7on/mushrommstreet/activity/EditAddresActivity.java
@@ -1,27 +1,11 @@
 package com.cyq7on.mushrommstreet.activity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.cyq7on.mushrommstreet.R;
-import com.cyq7on.mushrommstreet.bean.ShoppingDetailVo;
 import com.cyq7on.mushrommstreet.view.TitleBar;
-import com.cyq7on.mushroomstreet.AppConfig;
-import com.nostra13.universalimageloader.core.ImageLoader;
 
 /**
  * 
@@ -33,43 +17,21 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 * @version V1.0
  */
 public class EditAddresActivity extends BaseActivity {
-	private ListView listView;
-	private List<ShoppingDetailVo> dataList = new ArrayList<ShoppingDetailVo>();
-	private Orderdapter oederAdapter;
-	private TextView tvAllPrice;
-	private float priceAll = 0;
-	private RelativeLayout rlInfo;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_confirmorder);
+		setContentView(R.layout.activity_editaddress);
 		initData();
 		initView();
 	}
 
 	public void initData() {
-		// 获取购物车商品
-		dataList.addAll(AppConfig.goodsList);
-		for (int i = 0; i < dataList.size(); i++) {
-			priceAll += dataList.get(i).getPriceAll();
-		}
 	}
 	
 	@Override
 	public void initView() {
 		titleBar = (TitleBar) findViewById(R.id.title_bar);
-		tvAllPrice = (TextView) findViewById(R.id.tv_allprice);
-		listView = (ListView) findViewById(R.id.listview);
-		rlInfo = (RelativeLayout) findViewById(R.id.rl_info);
-		rlInfo.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-			}
-		});
-		tvAllPrice.setText("合计：￥" + priceAll);
 		titleBar.setTitle("编辑地址");
 		titleBar.setRightText("完成");
 		titleBar.setRightVisible();
@@ -77,166 +39,16 @@ public class EditAddresActivity extends BaseActivity {
 			
 			@Override
 			public void onClick(View v) {
-				
+				finish();
 			}
 		});
-		oederAdapter = new Orderdapter();
-		listView.setAdapter(oederAdapter);
 	}
 
 
-	private class Orderdapter extends BaseAdapter {
-
-		@Override
-		public int getCount() {
-			// TODO Auto-generated method stub
-			return dataList.size();
-		}
-
-		@Override
-		public Object getItem(int arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public long getItemId(int position) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public View getView(final int position, View convertView,
-				ViewGroup parent) {
-			final ViewHolder vh;
-			if (convertView == null) {
-				vh = new ViewHolder();
-				convertView = LayoutInflater.from(EditAddresActivity.this)
-						.inflate(R.layout.item_confirmorder, null);
-				vh.iv = (ImageView) convertView.findViewById(R.id.iv);
-				vh.tvName = (TextView) convertView.findViewById(R.id.tv_name);
-				vh.tvColor = (TextView) convertView.findViewById(R.id.tv_color);
-				vh.tvSize = (TextView) convertView.findViewById(R.id.tv_size);
-				vh.tvPriceOld = (TextView) convertView
-						.findViewById(R.id.tv_price_old);
-				vh.tvPirceNow = (TextView) convertView
-						.findViewById(R.id.tv_price_now);
-				vh.tvCount = (TextView) convertView.findViewById(R.id.tv_count);
-//				vh.btnSub = (Button) convertView.findViewById(R.id.btn_sub);
-//				vh.btnPlus = (Button) convertView.findViewById(R.id.btn_plus);
-				vh.btnChat = (Button) convertView.findViewById(R.id.btn_chat);
-				vh.tvFreight = (TextView) convertView
-						.findViewById(R.id.tv_freight);
-				vh.tvPirceAll = (TextView) convertView
-						.findViewById(R.id.tv_priceall);
-				vh.tvStore = (TextView) convertView.findViewById(R.id.tv_store);
-				vh.et = (EditText) convertView.findViewById(R.id.et);
-				convertView.setTag(vh);
-			} else {
-				vh = (ViewHolder) convertView.getTag();
-			}
-			if (dataList != null && dataList.size() > 0) {
-				ShoppingDetailVo vo = dataList.get(position);
-				ImageLoader.getInstance().displayImage(vo.getImageUrl(), vh.iv,
-						AppConfig.options);
-				vh.tvName.setText(vo.getName());
-				vh.tvColor.setText("颜色：" + vo.getColor());
-				vh.tvSize.setText("尺码：" + vo.getSize());
-				vh.tvPriceOld.setText("¥" + vo.getPriceOld());
-				vh.tvPirceNow.setText("¥" + vo.getPriceNow());
-				if ("0".equals(vo.getFreight())) {
-					vh.tvFreight.setText("全国包邮");
-				} else {
-					vh.tvFreight.setText("¥" + vo.getFreight());
-				}
-				vh.tvPirceAll.setText("¥" + vo.getPriceAll());
-				vh.tvCount.setText(vo.getCount() + "");
-//				vh.btnSub.setOnClickListener(new OnClickListener() {
-//
-//					@Override
-//					public void onClick(View v) {
-//						int count = Integer.parseInt(vh.tvCount.getText()
-//								.toString());
-//						if (count > 1) {
-//							count--;
-//							vh.tvCount.setText(count + "");
-//							String info = tvAllPrice.getText().toString()
-//									.substring(4);
-//							float price = Float.parseFloat(dataList.get(
-//									position).getPriceNow());
-//							float allPrice = Float.parseFloat(info);
-//							tvAllPrice.setText("合计：￥" + (allPrice - price));
-//						}
-//					}
-//				});
-//				vh.btnPlus.setOnClickListener(new OnClickListener() {
-//
-//					@Override
-//					public void onClick(View v) {
-//						int count = Integer.parseInt(vh.tvCount.getText()
-//								.toString());
-//						count++;
-//						vh.tvCount.setText(count + "");
-//						String info = tvAllPrice.getText().toString()
-//								.substring(4);
-//						float price = Float.parseFloat(dataList.get(position)
-//								.getPriceNow());
-//						float allPrice = Float.parseFloat(info);
-//						if (allPrice == 0) {
-//							tvAllPrice.setText("合计：￥" + count * price);
-//						} else {
-//							tvAllPrice.setText("合计：￥" + (allPrice + price));
-//						}
-//
-//					}
-//				});
-				vh.btnChat.setOnClickListener(new OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-
-					}
-				});
-				vh.tvStore.setText(vo.getStoreName());
-			}
-
-			return convertView;
-		}
-	}
-
-	private class ViewHolder implements OnClickListener {
-		ImageView iv;
-		TextView tvName;
-		TextView tvColor;
-		TextView tvSize;
-		TextView tvPriceOld;
-		TextView tvPirceNow;
-		TextView tvFreight;
-		TextView tvPirceAll;
-		TextView tvCount;
-		TextView tvStore;
-//		Button btnSub;
-//		Button btnPlus;
-		Button btnChat;
-		EditText et;
-
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
-
-		}
-	}
 
 
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.btn_ordernow:
-			Intent intent = new Intent(this,CheckstandActivity.class);
-			String info = tvAllPrice.getText().toString()
-					.substring(4);
-			intent.putExtra("allPrice", info);
-			startActivity(intent);
-			break;
 		default:
 			break;
 		}
